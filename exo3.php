d7e8d0007a1153bacfd3104428cb94572e8da462
Anger romain <anger.romain@gmail.com>
2022-01-11 11:54:05 +0100

    exercice 1,2,3 terminé

---
+++ exo3.php
@@ -0,0 +1,19 @@
+<?php
+
+function Secu_verif($nbSecu){
+    $regex = '/^[12][0-9]{2}[0-1][0-9](2[AB]|[0-9]{2})[0-9]{3}[0-9]{3}$/x';
+    
+    if (preg_match($regex, $nbSecu)) {
+        print "La longeur du numero de sécurité social est correct";
+    }
+    else{
+        print "La longeur du numero de sécurité social n'est pas correct";
+    }
+    return;
+}
+
+// $nbSecu = 1870678029052;
+print "Veuillez entrer votre numero de sécurité social".PHP_EOL;
+$nbSecu = readline();
+print Secu_verif($nbSecu);
+?>
